62fa5c7b1cd4cc2d57fb8b473e0052973717dbf3
Jelloko <rainbow.samp@gmail.com>
2024-11-01 19:32:50 -0400

    imported InfoAlert and ErrorAlert from alert component and apllyed them to CitySearch and NumberOfEvents

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import { InfoAlert, ErrorAlert } from './components/Alert';
 import { extractLocations, getEvents } from './api';
 import './App.css';
 
@@ -12,6 +13,8 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
   const [error, setError] = useState(null); 
 
   const fetchData = async () => {
@@ -37,10 +40,14 @@ const App = () => {
 
   return (
     <div className="App">
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+      </div>
       {error && <p>Error: {error}</p>} {/* Display error message if any */}
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert} />
       <EventList events={events} />
-      <NumberOfEvents onNumberChange={setCurrentNOE} />
+      <NumberOfEvents onNumberChange={setCurrentNOE} setErrorAlert={setErrorAlert} />
     </div>
   );
 };
